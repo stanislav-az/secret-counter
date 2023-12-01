090694488abeb4e183fdf67e2f6100c279320b4f
stanislav-az <st.zhdanovich@gmail.com>
2023-12-01 17:26:26 +0700

    deploy contract code

---
+++ node/main.js
@@ -0,0 +1,50 @@
+import { SecretNetworkClient, Wallet } from "secretjs";
+import * as fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+
+const wallet = new Wallet(process.env.MNEMONIC);
+
+const contract_wasm = fs.readFileSync("../contract.wasm.gz");
+
+const secretjs = new SecretNetworkClient({
+  chainId: "pulsar-3",
+  url: "https://api.pulsar.scrttestnet.com",
+  wallet: wallet,
+  walletAddress: wallet.address,
+});
+
+console.log("Owner wallet address: ", wallet.address);
+// console.log(secretjs);
+
+let upload_contract = async () => {
+  let tx = await secretjs.tx.compute.storeCode(
+    {
+      sender: wallet.address,
+      wasm_byte_code: contract_wasm,
+      source: "",
+      builder: "",
+    },
+    {
+      gasLimit: 4_000_000,
+    }
+  );
+
+  // console.log(tx);
+
+  const codeId = Number(
+    tx.arrayLog.find((log) => log.type === "message" && log.key === "code_id")
+      .value
+  );
+
+  console.log("codeId: ", codeId);
+
+  const contractCodeHash = (
+    await secretjs.query.compute.codeHashByCodeId({ code_id: codeId })
+  ).code_hash;
+  console.log(`Contract hash: ${contractCodeHash}`);
+};
+
+// // codeId:  2558
+// // Contract hash: 7bc3ac7437126da306a605c0e9e0fa0f43b8d383fc2e88ed95530f3628c24730
+// await upload_contract();
